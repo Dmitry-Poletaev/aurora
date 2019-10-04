509576572ff2349e343d8183c595aac144610eb9
Vasua Pupkin <you@gmail.com>
2019-10-04 17:43:36 +0300

    settings

--- auroraonline/settings.py
+++ auroraonline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p-u2mek&khdleghb$mzu1#3n(x+sa2xzm_ee%^dn-rws%glg!7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['auroraonline.herokuapp','localhost']
+ALLOWED_HOSTS = ['auroraonline.herokuapp.com'','localhost']
 
 
 # Application definition

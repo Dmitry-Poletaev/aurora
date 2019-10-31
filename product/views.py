2ee96d3aef982ec3bb5b3056c4a4ecc92c57075c
Vasua Pupkin <you@gmail.com>
2019-10-31 16:07:05 +0300

    cart

--- product/views.py
+++ product/views.py
@@ -18,7 +18,6 @@ def main_page(request):
 
 def category_view(request, slug):
     category = Category.objects.get(slug=slug)
-    #products = category.product_set.all()
     products = Product.objects.filter(category=category)
     categories = Category.objects.all()
 

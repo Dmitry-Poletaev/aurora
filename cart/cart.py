b517d747b83edb93dbbd68c80549af1377d4676c
Vasua Pupkin <you@gmail.com>
2020-01-17 10:26:19 +0300

    comments

--- cart/cart.py
+++ cart/cart.py
@@ -7,22 +7,21 @@ class Cart(object):
 
     def __init__(self, request):
         """
-        Initialize the cart.
+        Инициализируем корзину
         """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # save an empty cart in the session
+            # сохраняем пустую корзину в сесии
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def __iter__(self):
         """
-        Iterate over the items in the cart and get the products 
-        from the database.
+        Итерируемся по товарам в корзине и получаем товары из бд
         """
         product_ids = self.cart.keys()
-        # get the product objects and add them to the cart
+        # получаем объект товара и добавляем его в корзину
         products = Product.objects.filter(id__in=product_ids)
 
         cart = self.cart.copy()
@@ -36,18 +35,18 @@ class Cart(object):
     
     def __len__(self):
         """
-        Count all items in the cart.
+        Считаем весь товар корзине
         """
         return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, update_quantity=False):
         """
-        Add a product to the cart or update its quantity.
+        Добавляем товар в корзину или обновляем его количество.
         """
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,
-                                      'price': str(product.price)}
+                                        'price': str(product.price)}
         if update_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -55,12 +54,12 @@ class Cart(object):
         self.save()
 
     def save(self):
-        # mark the session as "modified" to make sure it gets saved
+        # помечаем сессию как "modified" ,чтобы убедиться, что она сохранена
         self.session.modified = True
 
     def remove(self, product):
         """
-        Remove a product from the cart.
+        Удаляем товар из корзины
         """
         product_id = str(product.id)
         if product_id in self.cart:
@@ -71,7 +70,7 @@ class Cart(object):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
     def clear(self):
-        # remove cart from session
+        # удаляем товар из сессии
         del self.session[settings.CART_SESSION_ID]
         self.save()
         
